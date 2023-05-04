0f3471224b756d7e573495d77c7bef0d543446e4
해솔Haesol <sangho.lee.1990@gmail.com>
2023-05-04 12:53:34 +0900

    commit

--- src/TalentPlatform-LSH0419.R
+++ src/TalentPlatform-LSH0419.R
@@ -278,7 +278,8 @@ dataL1 %>%
 
 # ▶ 특성11 높음, 보통, 낮음 을 명목변수 3개로 분류하여
 # 각 집단 (높음,보통,낮음) 간 적응특성4 요인 점수를 비교하는 그래프로 변경 부탁드립니다.
-dataList = list(allData = allData, subData = subData)
+# dataList = list(allData = allData, subData = subData)
+dataList = list(allData = allData)
 
 nameInfo = "allData"
 
@@ -306,19 +307,27 @@ dataL1$type = forcats::fct_relevel(dataL1$type, c("낮음", "보통", "높음"))
 plotSubTitle = sprintf("%s", "특성11 집단에 따른 적응특성4 점수 분포")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
 
-makePlot = ggplot(dataL1, aes(x = key, y = 적응특성4, fill = key)) +
+makePlot = ggplot(dataL1, aes(x = type, y = 적응특성4, fill = type)) +
   geom_boxplot(size = 0.5) +
   geom_jitter(alpha = 0.2) +
+  stat_summary(fun = mean, geom = "point", shape = 23, size = 5, fill = "white", aes(group = type)) +
   labs(x = NULL, y = "적응특성4 점수", fill = NULL, subtitle = plotSubTitle) +
   theme(
     text = element_text(size = 16)
-    , axis.text.x = element_text(angle = 45, hjust = 1)
+    # , axis.text.x = element_text(angle = 45, hjust = 1)
     , legend.position = "top"
-  ) +
-  facet_wrap(~type, scale = "free_x")
+  )
+  # facet_wrap(~type, scale = "free_x")
 
 ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+dataL1 %>%
+  dplyr::group_by(type) %>%
+  dplyr::summarise(
+    meanVal = mean(적응특성4, na.rm = TRUE)
+  )
+
 
 # ****************************************************************************
 # 3번
@@ -386,6 +395,12 @@ makePlot = ComplexUpset::upset(
 ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
+# 1. 거리가 가까워서
+# 2. 학비가 저렴하거나 장학금 혜택 때문에
+# 3. 전공에 비전이 있다고 생각해서
+# 4. 동기 및 선후배들이 좋아서
+# 5. 별대른 대안이 없어서
+# 6. 기타
 
 #
 # modelRes = mirt::mirt(dataL1, model = 1, itemtype = "Rasch")
@@ -430,14 +445,6 @@ cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 # 도표에서 빨간색 점은 각 항목에 대한 항목 임계값(4개의 응답 범주를 구분하는 3개의 임계값)을 나타내고
 # 별표는 각 항목에 대한 임계값의 평균값을 나타냅니다. 이 임계값이 높을수록 더 많은 "팀워크"가 필요합니다. 플롯의 왼쪽에는 잠재 특성(즉, 팀워크 구성)의 분포도 표시됩니다.
 
-
-# 1. 거리가 가까워서
-# 2. 학비가 저렴하거나 장학금 혜택 때문에
-# 3. 전공에 비전이 있다고 생각해서
-# 4. 동기 및 선후배들이 좋아서
-# 5. 별대른 대안이 없어서
-# 6. 기타
-
 # plotSubTitle = sprintf("%s (%s)", "Q7.1 전체 및 우수군 집단에 따른 항목별 점수 분포", nameInfo)
 # saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
 #
@@ -475,6 +482,8 @@ data = allData %>%
 data$type = forcats::fct_relevel(data$type, c("낮음", "보통", "높음"))
 data$key = forcats::fct_relevel(data$key, c("낮음", "보통", "높음"))
 
+# keyInfo = "위기특성"
+# keyInfo = "적응특성"
 keyList = c("위기특성", "적응특성")
 for (keyInfo in keyList) {
 
@@ -498,6 +507,14 @@ for (keyInfo in keyList) {
   ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
   cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 }
+
+dataL1 %>%
+  dplyr::ungroup() %>%
+  # dplyr::group_by(type) %>%
+  dplyr::group_by(key) %>%
+  dplyr::summarise(
+    meanVal = mean(meanVal, na.rm = TRUE)
+  )
 
 # ****************************************************************************
 # 5번
@@ -526,6 +543,7 @@ data = allData %>%
   ) %>%
   dplyr::filter(
     type == "부적응군" | type2 == "위기군"
+    , Q12 == "없음"
   ) %>%
   tibble::rowid_to_column()
 
@@ -587,123 +605,9 @@ makePlot = ComplexUpset::upset(
 ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
-# 정성적 데이터, 정량적 데이터의 조합과 산포도 등을 동시에 플롯하여 데이터 세트의 특징을 탐색적으로 파악하는 데 편리한 "UpSetR" 패키지입니다.
-# 조금, 사용법에 버릇이 있습니다만 「사용 데이터 예를 최소 구성」에 출력예를 소개합니다.
-
-# # *****************************************
-# # 시각화
-# # *****************************************
-# plotSubTitle = sprintf("%s", "제품전후 전력량에 따른 비교추이")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# makePlot = ggpubr::ggscatter(
-#   data = dataL1, x = "id", y = "val", color = "key"
-#   , add = "reg.line", alpha = 0.3, palette = c("#00AFBB", "#E7B800")
-# ) +
-#   labs(title = NULL, x = "인덱스", y = "전력 사용량", color = NULL, subtitle = plotSubTitle) +
-#   theme_bw() +
-#   ggpubr::stat_regline_equation(aes(color = key), label.x.npc = 0.0, label.y.npc = 0.95, size = 6) +
-#   ggpubr::stat_cor(aes(color = key), label.x.npc = 0.5, label.y.npc = 0.95, p.accuracy = 0.01, r.accuracy = 0.01, size = 6) +
-#   theme(
-#     text = element_text(size = 18)
-#     , legend.position = "top"
-#   )
-#
-# ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
-# ggplot2::last_plot()
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#
-# plotSubTitle = sprintf("%s", "제품전후 전력량에 따른 밀도함수")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# makePlot = ggpubr::ggdensity(
-#   data = dataL1, x = "val", add = "mean", rug = TRUE,
-#   color = "key", fill = "key", palette = c("#00AFBB", "#E7B800")
-#   ) +
-#   labs(title = NULL, x = "전력 사용량", y = "밀도함수", color = NULL, subtitle = plotSubTitle) +
-#   theme(
-#     text = element_text(size = 18)
-#     , legend.position = "top"
-#   )
-#
-# ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
-# ggplot2::last_plot()
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#
-#
-# plotSubTitle = sprintf("%s", "제품전후 전력량에 따른 빈도분포")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# makePlot = ggpubr::gghistogram(
-#   data = dataL1, x = "val", add = "mean", rug = TRUE,
-#   color = "key", fill = "key", palette = c("#00AFBB", "#E7B800")
-# ) +
-#   labs(title = NULL, x = "전력 사용량", y = "밀도함수", color = NULL, subtitle = plotSubTitle) +
-#   theme(
-#     text = element_text(size = 18)
-#     , legend.position = "top"
-#   )
-#
-# ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
-# ggplot2::last_plot()
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#
-#
-# plotSubTitle = sprintf("%s", "제품전후 전력량에 따른 상자그림")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# makePlot = ggpubr::ggboxplot(
-#   dataL1, x = "key", y = "val", color = "key", palette =c("#00AFBB", "#E7B800"),
-#   add = "jitter", shape = "key", alpah = 0.1
-# ) +
-#   ggpubr::stat_compare_means(comparisons = list( c("before", "after"))) +
-#   labs(title = NULL, x = "전력 사용량", y = "밀도함수", color = NULL, subtitle = plotSubTitle) +
-#   theme(
-#     text = element_text(size = 18)
-#     , legend.position = "top"
-#   )
-#
-# ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
-# ggplot2::last_plot()
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#
-#
-# # *****************************************
-# # 통계 검정
-# # *****************************************
-# # F 검정
-# fTest = var.test(val ~ key, data = dataL1, conf.level = 0.95)
-# print(fTest)
-#
-# # F 검정에서 유의수준 p-value < 0.05 이하로서 귀무가설이 기각 (두 그룹은 분산 차이)
-# mainTitle = sprintf("%s", "제품전후 간의 F 검정")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# plot(fTest) +
-#   ggsave(filename = saveImg, width = 10, height = 6, dpi = 600)
-#
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#
-# # T 검정
-# # 등분산 가정 O
-# # tTest = t.test(val ~ key, data = dataL1, conf.level = 0.95, var.equal = TRUE, paired = FALSE)
-#
-# # 등분산 가정 X
-# tTest = t.test(val ~ key, data = dataL1, conf.level = 0.95, var.equal = FALSE, paired = FALSE)
-#
-# # T 검정에서 유의수준 p-value는 0.01 이하로서 귀무가설 기각 (두 그룹은 평균 차이)
-# print(tTest)
-#
-# mainTitle = sprintf("%s", "제품전후 간의 T 검정")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-# dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# plot(tTest) +
-#   ggsave(filename = saveImg, width = 10, height = 6, dpi = 600)
-#
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+data %>%
+  dplyr::group_by(type3) %>%
+  dplyr::summarise(
+    meanVal = mean(생활적응, na.rm = TRUE)
+    , meanVal2 = mean(심리적응, na.rm = TRUE)
+  )
