2db1183a21d38fd3f2da0c784a9ce2560de7a225
shlee1990 <sangho.lee.1990@gmail.com>
2023-05-27 18:01:57 +0900

    commit

--- src/TalentPlatform-LSH0426.R
+++ src/TalentPlatform-LSH0426.R
@@ -11,7 +11,7 @@
 #================================================
 # 요구사항
 #================================================
-# R을 이용한 대중교통 통행량 막대 그래프 시각화
+# R을 이용한 IPTW에 따른 kaplan-meier 시각화
 
 # ================================================
 # 초기 환경변수 설정
@@ -48,81 +48,6 @@ if (env == "local") {
 # 라이브러리 읽기
 library(tidyverse)
 library(xlsx)
-
-# 파일 읽기
-# fileList = Sys.glob(file.path(globalVar$inpPath, serviceName, "*.xlsx"))
-#
-# # fileInfo = fileList[1]
-# data = tibble::tibble()
-# for (fileInfo in fileList) {
-#
-#   selData = xlsx::read.xlsx(fileInfo, sheetIndex = 1) %>%
-#     tibble::as.tibble() %>%
-#     dplyr::filter(
-#       ! is.na(통행량)
-#     )
-#
-#   data = dplyr::bind_rows(data, selData)
-# }
-#
-#
-# dataL1 = data %>%
-#   dplyr::rename(key = 시도.출발.) %>%
-#   dplyr::select(c("일자", key, "통행량")) %>%
-#   dplyr::arrange(desc(통행량))
-#
-# statData = dataL1 %>%
-#   dplyr::group_by(일자) %>%
-#   dplyr::summarise(
-#     sumVal = sum(통행량, na.rm = TRUE)
-#   )
-#
-# statDataL1 = dataL1 %>%
-#   dplyr::left_join(statData, by = c("일자" = "일자")) %>%
-#   dplyr::mutate(
-#     perVal = (통행량 / sumVal) * 100.0
-#   ) %>%
-#   dplyr::arrange(desc(통행량))
-#
-#
-# # 내림차순 정렬
-# dataL1$key = forcats::fct_relevel(dataL1$key, unique(dataL1$key))
-# statDataL1$key = forcats::fct_relevel(statDataL1$key, unique(statDataL1$key))
-#
-#
-# # 주요 시도에 따른 대중교통 통행량 막대 그래프
-# plotSubTitle = sprintf("%s", "주요 시도에 따른 대중교통 통행량 막대 그래프")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# ggplot(data = dataL1, aes(x = key, y = 통행량, fill = 일자, label = round(통행량, 0))) +
-#   geom_bar(stat = "identity", width = 0.5, position=position_dodge(width = 0.75))  +
-#   geom_text(position = position_dodge(width = 0.75), size = 4, vjust = -0.5, hjust = 0.5, color = "black") +
-#   labs(title = NULL, x = "주요 시도", y = "통행량", colour = NULL, fill = NULL, subtitle = plotSubTitle) +
-#   theme(
-#     text = element_text(size = 16)
-#     , legend.position = "top"
-#   ) +
-#   ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
-#
-#
-# # 주요 시도에 따른 대중교통 %통행량 막대 그래프
-# plotSubTitle = sprintf("%s", "주요 시도에 따른 대중교통 %통행량 막대 그래프")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
-# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-#
-# ggplot(data = statDataL1, aes(x = key, y = perVal, fill = 일자, label = round(perVal, 1))) +
-#   geom_bar(stat = "identity", width = 0.5, position=position_dodge(width = 0.75))  +
-#   geom_text(position = position_dodge(width = 0.75), size = 4, vjust = -0.5, hjust = 0.5, color = "black") +
-#   labs(title = NULL, x = "주요 시도", y = "%통행량", colour = NULL, fill = NULL, subtitle = plotSubTitle) +
-#   theme(
-#     text = element_text(size = 16)
-#     , legend.position = "top"
-#   ) +
-#   ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
-
-
-# 라이브러리 읽기
 library(rms)
 library(PredictABEL)
 library(dplyr)
@@ -165,6 +90,8 @@ library(MatchIt)
 library(MatchIt)
 library(survival)
 
+
+
 ################ raw data ################
 ##########################################
 ##########################################
@@ -172,125 +99,125 @@ library(survival)
 # PTRG <-read.csv(file="C:/Users/User/Desktop/PTRG_DES/data/PTRG_DES_20220723_update.csv",header=TRUE)
 
 # 파일 읽기
-fileList = Sys.glob(file.path(globalVar$inpPath, serviceName, "PTRG_DES_20220723_update.csv"))
-fileInfo = fileList[1]
-
-data <- read.csv(file = fileInfo, header = TRUE)
-PTRG <- read.csv(file = fileInfo, header = TRUE)
-
-head(PTRG)
-str(PTRG)
-nrow(PTRG)
-
-### before PSM/IPTW table 1.
-
-mytable(CCB ~ ., data = PTRG, method = 1, catMethod = 0)
-mycsv(mytable(CCB ~ ., data = PTRG, method = 1, catMethod = 0), file = "beforePSM.csv")
-
-
-##### before PSM/IPTW table 2. #####
-
-
-cox.MACCE = coxph(Surv(MACCE2_d, MACCE2 == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(Death_d, CV_DEATH == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(MI_d, TOTAL_MI == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(ST_d, ST_YN == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(CVA_d, CVA == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(RR_d, Revascularization == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
-
-cox.MACCE = coxph(Surv(BL_day, BL_YN == 1) ~ CCB, data = PTRG)
-summary(cox.MACCE)
+# fileList = Sys.glob(file.path(globalVar$inpPath, serviceName, "PTRG_DES_20220723_update.csv"))
+# fileInfo = fileList[1]
+# 
+# data <- read.csv(file = fileInfo, header = TRUE)
+# PTRG <- read.csv(file = fileInfo, header = TRUE)
+# 
+# head(PTRG)
+# str(PTRG)
+# nrow(PTRG)
+# 
+# ### before PSM/IPTW table 1.
+# 
+# mytable(CCB ~ ., data = PTRG, method = 1, catMethod = 0)
+# mycsv(mytable(CCB ~ ., data = PTRG, method = 1, catMethod = 0), file = "beforePSM.csv")
+# 
+# 
+# ##### before PSM/IPTW table 2. #####
+# 
+# 
+# cox.MACCE = coxph(Surv(MACCE2_d, MACCE2 == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(Death_d, CV_DEATH == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(MI_d, TOTAL_MI == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(ST_d, ST_YN == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(CVA_d, CVA == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(RR_d, Revascularization == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
+# 
+# cox.MACCE = coxph(Surv(BL_day, BL_YN == 1) ~ CCB, data = PTRG)
+# summary(cox.MACCE)
 
 
 ##################### Figure 1. before PSM/IPTW
 
-fit <- survfit(Surv(MACCE2_d, MACCE2 == 1) ~ CCB, data = PTRG)
-
-ggsurvplot(fit, title = "MACCE2", data = PTRG,
-           fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.2), break.by = 0.05)
-
-fit <- survfit(Surv(Death_d, CV_DEATH == 1) ~ CCB, data = PTRG)
-fit
-ggsurvplot(fit, title = "CV death", data = PTRG,
-           fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.1), break.by = 0.05)
-
-fit <- survfit(Surv(MI_d, TOTAL_MI == 1) ~ CCB, data = PTRG)
-fit
-ggsurvplot(fit, title = "Myocardial infarction", data = PTRG,
-           fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.1), break.by = 0.05)
-
-fit <- survfit(Surv(ST_d, ST_YN == 1) ~ CCB, data = PTRG)
-fit
-ggsurvplot(fit, title = "Stent thrombosis", data = PTRG,
-           fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.1), break.by = 0.05)
-
-fit <- survfit(Surv(CVA_d, CVA == 1) ~ CCB, data = PTRG)
-fit
-ggsurvplot(fit, title = "Stroke", data = PTRG,
-           fun = "cumhaz",
-           risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.1), break.by = 0.05)
-
-fit <- survfit(Surv(BL_day, BL_YN == 1) ~ CCB, data = PTRG)
-fit
-ggsurvplot(fit, title = "Bleeding", data = PTRG,
-           fun = "cumhaz",
-           risk.table = TRUE, conf.int = FALSE,
-           legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-           xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-           ylim = c(0, 0.1), break.by = 0.05)
-
-
-#### univariable cox
-
-cox.MACCE <- coxph(Surv(MACCE2_d, MACCE2 == 1) ~ TC, data = PTRG)
-summary(cox.MACCE)
-
-
-cox.MACCE <- coxph(Surv(MACCE2_d, MACCE2 == 1) ~ CCB +
-  Age +
-  BMI +
-  DM +
-  HTN +
-  CKD +
-  CHF +
-  PRE_CVA +
-  HB +
-  TC +
-  ASP +
-  CILO +
-  BB +
-  STATIN +
-  ACCAHA +
-  Bifurcation +
-  MultivvDz +
-  TotalSTL, data = PTRG)
-summary(cox.MACCE, max.risk = Inf)
+# fit <- survfit(Surv(MACCE2_d, MACCE2 == 1) ~ CCB, data = PTRG)
+# 
+# ggsurvplot(fit, title = "MACCE2", data = PTRG,
+#            fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.2), break.by = 0.05)
+# 
+# fit <- survfit(Surv(Death_d, CV_DEATH == 1) ~ CCB, data = PTRG)
+# fit
+# ggsurvplot(fit, title = "CV death", data = PTRG,
+#            fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.1), break.by = 0.05)
+# 
+# fit <- survfit(Surv(MI_d, TOTAL_MI == 1) ~ CCB, data = PTRG)
+# fit
+# ggsurvplot(fit, title = "Myocardial infarction", data = PTRG,
+#            fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.1), break.by = 0.05)
+# 
+# fit <- survfit(Surv(ST_d, ST_YN == 1) ~ CCB, data = PTRG)
+# fit
+# ggsurvplot(fit, title = "Stent thrombosis", data = PTRG,
+#            fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.1), break.by = 0.05)
+# 
+# fit <- survfit(Surv(CVA_d, CVA == 1) ~ CCB, data = PTRG)
+# fit
+# ggsurvplot(fit, title = "Stroke", data = PTRG,
+#            fun = "cumhaz",
+#            risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.1), break.by = 0.05)
+# 
+# fit <- survfit(Surv(BL_day, BL_YN == 1) ~ CCB, data = PTRG)
+# fit
+# ggsurvplot(fit, title = "Bleeding", data = PTRG,
+#            fun = "cumhaz",
+#            risk.table = TRUE, conf.int = FALSE,
+#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
+#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
+#            ylim = c(0, 0.1), break.by = 0.05)
+# 
+# 
+# #### univariable cox
+# 
+# cox.MACCE <- coxph(Surv(MACCE2_d, MACCE2 == 1) ~ TC, data = PTRG)
+# summary(cox.MACCE)
+# 
+# 
+# cox.MACCE <- coxph(Surv(MACCE2_d, MACCE2 == 1) ~ CCB +
+#   Age +
+#   BMI +
+#   DM +
+#   HTN +
+#   CKD +
+#   CHF +
+#   PRE_CVA +
+#   HB +
+#   TC +
+#   ASP +
+#   CILO +
+#   BB +
+#   STATIN +
+#   ACCAHA +
+#   Bifurcation +
+#   MultivvDz +
+#   TotalSTL, data = PTRG)
+# summary(cox.MACCE, max.risk = Inf)
 
 
 #
@@ -456,8 +383,6 @@ data <- read.csv(file = fileInfo)
 # table(data$CCB)
 # temp = data[, c("No", "Age", "Sex", "BMI", "Dx", "Smoking", "HTN", "DM", "Dyslipid", "CKD", "PAD", "CHF", "PRE_MI", "PRE_PCI", "PRE_CVA", "VERI_PRU", "LVEF", "HB", "Cr", "LDL", "ACCAHA", "ASP", "CILO", "RASinh", "BB", "CCB", "STATIN", "CHIP_CRITERIA")]
 # temp = data[, c("No", "Age", "Sex", "BMI", "Dx", "AMI", "Smoking", "DM", "HTN", "Dyslipid", "CKD", "PAD", "CHF", "PRE_MI", "PRE_PCI", "PRE_CABG", "PRE_CVA", "VERI_PRU", "LVEF", "WBC", "HB", "PLATELET", "Cr", "TC", "TG", "HDL", "LDL", "hsCRP", "Hba1c", "ACCAHA", "TotalNoST", "Bifurcation", "CTO", "ASP", "CILO", "RASinh", "BB", "CCB", "STATIN", "PPI", "CHIP_CRITERIA")]
-
-
 # covariates = c("No", "Age", "Sex", "BMI", "Dx", "AMI", "Smoking", "DM", "HTN", "Dyslipid", "CKD", "PAD", "CHF", "PRE_MI", "PRE_PCI", "PRE_CABG", "PRE_CVA", "VERI_PRU", "LVEF", "WBC", "HB", "PLATELET", "Cr", "TC", "TG", "HDL", "LDL", "hsCRP", "Hba1c", "ACCAHA", "TotalNoST", "Bifurcation", "CTO", "ASP", "CILO", "RASinh", "BB", "CCB", "STATIN", "PPI", "CHIP_CRITERIA")
 
 temp = data %>%
@@ -631,38 +556,61 @@ nrow(data_iptw)
 #            ylim = c(0, 0.1), break.by = 0.05)
 
 
-# create a survival object
-Surv_obj = Surv(data_iptw$MACCE2_d, data_iptw$MACCE2 == 1)
-
-# fit a Cox model
+# ******************************************************************************
+# 2023.05.27 가중치/비가중치
+# # create a survival object
+# Surv_obj = Surv(data_iptw$MACCE2_d, data_iptw$MACCE2 == 1)
+# 
+# # fit a Cox model
 # fit_unweighted = coxph(Surv_obj ~ CCB, data = data_iptw)
-
+# 
 # create weights
-weights = ifelse(data_iptw$CCB == 1, 1 / temp$ipw, 1)
-
-# fit a weighted Cox model
-fit_weighted = coxph(Surv_obj ~ CCB, data = data_iptw, weights = weights)
-
-# generate Kaplan-Meier curves
-kmfit_weighted = survfit(fit_weighted)
+# weights = ifelse(data_iptw$CCB == 1, 1 / temp$ipw, 1)
+# 
+# # fit a weighted Cox model
+# fit_weighted = coxph(Surv_obj ~ CCB, data = data_iptw, weights = weights)
+# 
+# # generate Kaplan-Meier curves
+# kmfit_weighted = survfit(fit_weighted)
 # kmfit_unweighted = survfit(fit_unweighted)
+# 
+# fits = list("weighted" = kmfit_weighted, "unweighted" = kmfit_unweighted)
 
-fits = list("weighted" = kmfit_weighted, "unweighted" = kmfit_unweighted)
+# plotSubTitle = sprintf("%s", "IPTW에 따른 kaplan-meier 곡선")
+# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
+# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
+# 
+# makePlot = survminer::ggsurvplot(fits, title = "Bleeding", data = data_iptw,
+#                                  fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
+#                                  palette = c("gray 39", "orangered"), xlim = c(0, 1825),
+#                                  break.time.by = 365, xlab = "Follow-up duration (days)",
+#                                  ylim = c(0, 0.1), break.by = 0.05, combine = TRUE)
+# 
+# mainPlot = cowplot::plot_grid(makePlot$plot, makePlot$table, align = "v", ncol = 1, rel_heights = c(3, 1))
+# ggsave(plot = mainPlot, filename = saveImg, width = 10, height = 8, dpi = 600)
+# ******************************************************************************
 
+# ******************************************************************************
+# 2023.05.27 가중치/비가중치
+# create a survival object separately for each group
+Surv_obj_CCB = Surv(data_iptw[data_iptw$CCB == 1,]$MACCE2_d, data_iptw[data_iptw$CCB == 1,]$MACCE2 == 1)
+Surv_obj_noCCB = Surv(data_iptw[data_iptw$CCB == 0,]$MACCE2_d, data_iptw[data_iptw$CCB == 0,]$MACCE2 == 1)
 
-# fit <- survfit(Surv(MACCE2_d, MACCE2 == 1) ~ CCB, data = temp_2)
-# fit
-# ggsurvplot(fit, title = "MACCE2", data = temp_2,
-#            fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
-#            legend = "right", legend.labs = c("no CCB", "CCB"), palette = c("gray 39", "orangered"),
-#            xlim = c(0, 1825), break.time.by = 365, xlab = "Follow-up duration (days)",
-#            ylim = c(0, 0.2), break.by = 0.05)
+# Fit a weighted Cox model for each group
+fit_CCB = coxph(Surv_obj_CCB ~ CCB, data = data_iptw[data_iptw$CCB == 1, ], weights = weights[data_iptw$CCB == 1])
+fit_noCCB = coxph(Surv_obj_noCCB ~ CCB, data = data_iptw[data_iptw$CCB == 0, ], weights = weights[data_iptw$CCB == 0])
+
+# Generate Kaplan-Meier curves for each group
+kmfit_CCB = survfit(fit_CCB)
+kmfit_noCCB = survfit(fit_noCCB)
+
+fits = list("CCB" = kmfit_CCB, "no CCB" = kmfit_noCCB)
 
 plotSubTitle = sprintf("%s", "IPTW에 따른 kaplan-meier 곡선")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
 dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
 
-makePlot = survminer::ggsurvplot(fits, title = "Bleeding", data = weighteddata,
+makePlot = survminer::ggsurvplot(fits, title = "Bleeding", data = data_iptw,
                                  fun = "cumhaz", risk.table = TRUE, conf.int = FALSE,
                                  palette = c("gray 39", "orangered"), xlim = c(0, 1825),
                                  break.time.by = 365, xlab = "Follow-up duration (days)",
@@ -670,3 +618,4 @@ makePlot = survminer::ggsurvplot(fits, title = "Bleeding", data = weighteddata,
 
 mainPlot = cowplot::plot_grid(makePlot$plot, makePlot$table, align = "v", ncol = 1, rel_heights = c(3, 1))
 ggsave(plot = mainPlot, filename = saveImg, width = 10, height = 8, dpi = 600)
+# ******************************************************************************
