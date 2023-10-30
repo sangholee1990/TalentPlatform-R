5e4c07bfe583e82fb402da34dbb741e20846ed4b
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2023-10-31 02:43:09 +0900

    Update TalentPlatform-LSH0487.R

--- src/TalentPlatform-LSH0487.R
+++ src/TalentPlatform-LSH0487.R
@@ -99,9 +99,9 @@ dataL1 = data %>%
     )
   )
 
-# =================================================================
+# =======================================================================================================
 # 1. 전국 업종별 일반음식점 사업체 수(2023년 기준)
-# =================================================================
+# =======================================================================================================
 # 비중이 높은 순으로 10개 업종만 막대그래프로 시각화
 dataL2 = dataL1 %>%
   dplyr::filter(
@@ -227,14 +227,15 @@ ggplot(data = dataL6) +
 
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
-
+# =======================================================================================================
 # 2. 10대 또는 5대 업종의 지역별 분포
 # 지역별 비교가 목적이기 때문에, 2023년 기준 17개 시도 지역별 업종의 비중 비교(원형그래프 시각화)
+# =======================================================================================================
 # sidoAddrInfo = sidoAddrList[1]
 # sidoAddrInfo = "대전"
 sidoAddrList = dataL1$sidoAddr %>% unique() %>% sort()
 for (sidoAddrInfo in sidoAddrList) {
-
+  
   dataL2 = dataL1 %>%
     dplyr::filter(
       인허가일자 >= as.Date("2023-01-01")
@@ -247,18 +248,18 @@ for (sidoAddrInfo in sidoAddrList) {
     na.omit() %>%
     dplyr::arrange(desc(cnt)) %>%
     dplyr::slice(1:10)
-
+  
   # 정렬
   dataL2$업태구분명 = forcats::fct_relevel(dataL2$업태구분명, dataL2$업태구분명)
-
+  
   dataL3 = dataL2 %>%
     dplyr::mutate(per = (cnt / sum(dataL2$cnt, na.rm = TRUE)) * 100) %>%
     dplyr::mutate(name = sprintf("%s\n(%s %%)", 업태구분명, round(per, 1)))
-
+  
   mainTitle = sprintf("2023년 %s 업종별 일반음식점 사업체 %s", sidoAddrInfo, "원형그래프")
   saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
   dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
+  
   makePlot = ggplot(dataL3, aes(x = '', y = per, fill = 업태구분명)) +
     geom_bar(stat = 'identity', alpha = 0.6) +
     coord_polar('y', start = 0) +
@@ -273,19 +274,20 @@ for (sidoAddrInfo in sidoAddrList) {
       , axis.ticks = element_blank()
       , legend.position = "none"
     )
-
+  
   ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
   cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 }
 
 
-# 3. 시계열자료로는 특정 세부업종으로, 치킨통닭과 김밥(도시락) 냉면집 이렇게 3개가 필요한데요, 
+# =======================================================================================================
+# 3. 시계열자료로는 특정 세부업종으로, 치킨통닭과 김밥(도시락) 냉면집 이렇게 3개가 필요한데요,
 # 최근 10년간(2013년~2023년) 개폐업 현황을 막대그래프로 시각화
 # 단위는 전국, 서울, 대전, 세종 이렇게 가능할지 검토 부탁드립니다.
-# 3번 관련해서는, CSV자료에 영업기간도 도출할 수 있는데, 
+# 3번 관련해서는, CSV자료에 영업기간도 도출할 수 있는데,
 # 통닭(치킨), 김밥(도시락), 냉면집
 # 서울, 대전, 세종, 전국
-
+# =======================================================================================================
 typeList = c("통닭(치킨)", "김밥(도시락)", "냉면집")
 sidoList = c("서울", "대전", "세종")
 
@@ -293,7 +295,7 @@ sidoList = c("서울", "대전", "세종")
 # sidoInfo = sidoList[1]
 for (typeInfo in typeList) {
   for (sidoInfo in sidoList) {
-
+    
     dataL2 = dataL1 %>%
       dplyr::filter(
         업태구분명 == typeInfo
@@ -309,21 +311,22 @@ for (typeInfo in typeList) {
       ) %>%
       na.omit() %>%
       dplyr::arrange(dtYear)
-
+    
     mainTitle = sprintf("2013~2023년 %s (%s) 개폐업 현황", typeInfo, sidoInfo)
     saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
     dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
-    makePlot = ggplot(dataL2, aes(x = dtYear, y = cnt, fill = 영업상태명)) +
-      geom_bar(stat = "identity", width = 1.0, position = position_dodge(width = 1.0), alpha = 0.6) +
-      ggrepel::geom_text_repel(aes(label = scales::comma(cnt)), position = position_dodge(width = 1.0), color = "white", vjust = -0.5, size = 4, alpha = 0.8) +
+    
+    makePlot = ggplot(dataL2, aes(x = dtYear, y = cnt, color = 영업상태명)) +
+      geom_line() +
+      geom_point() +
+      ggrepel::geom_text_repel(aes(label = scales::comma(cnt)), vjust = -1.0, size = 4, show.legend = FALSE) +
       scale_x_continuous(minor_breaks = 2013:2023, breaks = 2013:2023) +
-      labs(x = "연도 ", y = "개폐업 수", fill = NULL, title = NULL, subtitle = mainTitle) +
+      labs(x = "연도 ", y = "개폐업 수", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
       theme(
         text = element_text(size = 16)
         , legend.position = "top"
       )
-
+    
     ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
     cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
   }
@@ -345,30 +348,33 @@ for (typeInfo in typeList) {
     ) %>%
     na.omit() %>%
     dplyr::arrange(dtYear)
-
+  
   mainTitle = sprintf("2013~2023년 %s (%s) 개폐업 현황", typeInfo, "전국")
   saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
   dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
-  makePlot = ggplot(dataL2, aes(x = dtYear, y = cnt, fill = 영업상태명)) +
-    geom_bar(stat = "identity", width = 1.0, position = position_dodge(width = 1.0)) +
-    ggrepel::geom_text_repel(aes(label = scales::comma(cnt)), position = position_dodge(width = 1.0), color = "white", vjust = -0.5, size = 4) +
+  
+  makePlot = ggplot(dataL2, aes(x = dtYear, y = cnt, color = 영업상태명)) +
+    geom_line() +
+    geom_point() +
+    ggrepel::geom_text_repel(aes(label = scales::comma(cnt)), vjust = -1.0, size = 4, show.legend = FALSE) +
     scale_x_continuous(minor_breaks = 2013:2023, breaks = 2013:2023) +
-    labs(x = "연도 ", y = "개폐업 수", fill = NULL, title = NULL, subtitle = mainTitle) +
+    labs(x = "연도 ", y = "개폐업 수", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
     theme(
       text = element_text(size = 16)
       , legend.position = "top"
     )
-
+  
   ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
   cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 }
 
-
-# 혹시 세부업종+특정 지역 결합해서 그 지역의 폐업업종 중 영업기간으로 분류하여 시각화할 수 있을지도 검토 부탁드립니다
+# =======================================================================================================
+# 혹시 세부업종+특정 지역 결합해서 그 지역의 폐업업종 중 영업기간으로 분류하여 
+# 시각화할 수 있을지도 검토 부탁드립니다
+# =======================================================================================================
 for (typeInfo in typeList) {
   for (sidoInfo in sidoList) {
-
+    
     dataL2 = dataL1 %>%
       dplyr::filter(
         업태구분명 == typeInfo
@@ -388,31 +394,30 @@ for (typeInfo in typeList) {
           , TRUE ~ "9년 이상"
         )
       ) %>%
-      dplyr::group_by(dtYear, 영업상태명, name) %>%
+      dplyr::group_by(영업상태명, name) %>%
       dplyr::summarise(
         cnt = n()
       ) %>%
       na.omit() %>%
-      dplyr::arrange(dtYear)
-
+      dplyr::arrange(name)
+    
     mainTitle = sprintf("2013~2023년 %s (%s) 폐업기간 분류", typeInfo, sidoInfo)
     saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
     dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
+    
     # 정렬
     dataL2$name = forcats::fct_relevel(dataL2$name, c("1년 미만", "1-3년", "3-5년", "5-7년", "7-9년", "9년 이상"))
-
-    makePlot = ggplot(dataL2, aes(x = dtYear, y = cnt, color = name)) +
-      geom_line() +
-      geom_point() +
-      scale_x_continuous(minor_breaks = 2013:2023, breaks = 2013:2023) +
-      labs(x = "연도 ", y = "개폐업 수", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+    
+    makePlot = ggplot(dataL2, aes(x = name, y = cnt, fill = name)) +
+      geom_bar(stat = "identity", width = 0.75, position = position_dodge(width = 1.0), alpha = 0.6, show.legend = FALSE) +
+      ggrepel::geom_text_repel(aes(label = scales::comma(cnt)), position = position_dodge(width = 1.0), color = "white", vjust = 0.0, size = 5) +
+      labs(x = "폐업 기간 ", y = "개폐업 수", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
       theme(
         text = element_text(size = 16)
         , legend.position = "top"
       ) +
       guides(color = guide_legend(nrow = 1))
-
+    
     ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
     cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
   }
@@ -539,4 +544,3 @@ for (typeInfo in typeList) {
 #   
 #   cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 # }
-
