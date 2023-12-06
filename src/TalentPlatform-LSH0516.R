b9a6dcf6ebe56f6067b9ecd433141fa845079418
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2023-12-06 09:20:47 +0900

    Update TalentPlatform-LSH0516.R

--- src/TalentPlatform-LSH0516.R
+++ src/TalentPlatform-LSH0516.R
@@ -60,7 +60,7 @@ if (Sys.info()["sysname"] == "Windows") {
 
 # 파일 읽기
 # fileInfo = "C:/SYSTEMS/PROG/R/TalentPlatform-R/resources/input/test/LSH0516/20231205_★설문+결과_정리_0816.xlsx")
-ileInfo = Sys.glob(file.path(globalVar$inpPath, serviceName, "20231205_★설문+결과_정리_0816.xlsx"))
+fileInfo = Sys.glob(file.path(globalVar$inpPath, serviceName, "20231205_★설문+결과_정리_0816.xlsx"))
 
 data = openxlsx::read.xlsx(fileInfo) %>% 
   as.tibble()
@@ -110,7 +110,6 @@ dataL1 = data %>%
     "Group" = "type"
   ) 
 
-
 mainTitle = sprintf("%s", "시민 및 전문가에 따른 공간 하위요소")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
 dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
@@ -153,82 +152,131 @@ ggplot(dataL1, aes(x = type, y = val, fill = type, label = round(val, 2))) +
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
 
-# 전문가에 따른 상위 요소 비율
+# 응답자에 따른 공간 하위요소
 dataL1 = data %>% 
-  dplyr::filter(name == "상위 요소 비율")
+  dplyr::filter(name == "응답자에 따른 공간 하위요소") %>% 
+  dplyr::select(type, key, val) %>%
+  tidyr::spread(key = "key", value = "val") %>% 
+  dplyr::rename(
+    "Group" = "type"
+  ) 
 
-mainTitle = sprintf("%s", "전문가에 따른 상위 요소 비율")
+mainTitle = sprintf("%s", "응답자에 따른 공간 하위요소")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
 dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
 
-ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)))) +
-  geom_bar(stat = 'identity', alpha = 0.6) +
-  coord_polar('y', start = 0) +
-  ggrepel::geom_label_repel(position = position_stack(vjust = 0.5), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
-  labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
-  theme_minimal() +
-  theme_classic() +
-  theme(
-    text = element_text(size = 16)
-    , axis.line = element_blank()
-    , axis.text = element_blank()
-    , axis.ticks = element_blank()
-    , legend.position = "top"
-  ) +
-  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+ggradar::ggradar(
+  dataL1
+  , values.radar = c(0, 3.5, 7)
+  , legend.position = "bottom"
+  , grid.min = 0, grid.mid = 3.5, grid.max = 7
+  , font.radar = "malgun"
+) +
+  ggsave(filename = saveImg, width = 12, height = 10, dpi = 600)
 
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+
+# 응답자에 따른 행태 하위요소
+dataL1 = data %>% 
+  dplyr::filter(name == "응답자에 따른 행태 하위요소") %>% 
+  dplyr::select(type, key, val) %>%
+  tidyr::spread(key = "key", value = "val") %>% 
+  dplyr::rename(
+    "Group" = "type"
+  ) 
+
+mainTitle = sprintf("%s", "응답자에 따른 행태 하위요소")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggradar::ggradar(
+  dataL1
+  , values.radar = c(0, 1.5, 3)
+  , legend.position = "bottom"
+  , grid.min = 0, grid.mid = 1.5, grid.max = 3
+  , font.radar = "malgun"
+) +
+  ggsave(filename = saveImg, width = 12, height = 10, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+# 전문가에 따른 상위 요소 비율
+# dataL1 = data %>% 
+#   dplyr::filter(name == "상위 요소 비율")
+# 
+# mainTitle = sprintf("%s", "전문가에 따른 상위 요소 비율")
+# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
+# 
+# ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)))) +
+#   geom_bar(stat = 'identity', alpha = 0.6) +
+#   coord_polar('y', start = 0) +
+#   ggrepel::geom_label_repel(position = position_stack(vjust = 0.5), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
+#   labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
+#   theme_minimal() +
+#   theme_classic() +
+#   theme(
+#     text = element_text(size = 16)
+#     , axis.line = element_blank()
+#     , axis.text = element_blank()
+#     , axis.ticks = element_blank()
+#     , legend.position = "top"
+#   ) +
+#   ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+# 
+# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
 
 # 전문가에 따른 공간 하위요소 비율
-dataL1 = data %>% 
-  dplyr::filter(name == "공간 하위요소 비율")
-
-mainTitle = sprintf("%s", "전문가에 따른 공간 하위요소 비율")
-saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
-ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)) )) +
-  geom_bar(stat = 'identity', alpha = 0.6) +
-  coord_polar('y', start = 0) +
-  ggrepel::geom_label_repel(position = position_stack(vjust = 0.25), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
-  labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
-  theme_minimal() +
-  theme_classic() +
-  theme(
-    text = element_text(size = 16)
-    , axis.line = element_blank()
-    , axis.text = element_blank()
-    , axis.ticks = element_blank()
-    , legend.position = "top"
-  ) +
-  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
-
-cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+# dataL1 = data %>% 
+#   dplyr::filter(name == "공간 하위요소 비율")
+# 
+# mainTitle = sprintf("%s", "전문가에 따른 공간 하위요소 비율")
+# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
+# 
+# ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)) )) +
+#   geom_bar(stat = 'identity', alpha = 0.6) +
+#   coord_polar('y', start = 0) +
+#   ggrepel::geom_label_repel(position = position_stack(vjust = 0.25), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
+#   labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
+#   theme_minimal() +
+#   theme_classic() +
+#   theme(
+#     text = element_text(size = 16)
+#     , axis.line = element_blank()
+#     , axis.text = element_blank()
+#     , axis.ticks = element_blank()
+#     , legend.position = "top"
+#   ) +
+#   ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+# 
+# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
 
 # 전문가에 따른 행태 하위요소 비율
-dataL1 = data %>% 
-  dplyr::filter(name == "행태 하위요소 비율")
-
-mainTitle = sprintf("%s", "전문가에 따른 행태 하위요소 비율")
-saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-
-ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)) )) +
-  geom_bar(stat = 'identity', alpha = 0.6) +
-  coord_polar('y', start = 0) +
-  ggrepel::geom_label_repel(position = position_stack(vjust = 0.5), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
-  labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
-  theme_minimal() +
-  theme_classic() +
-  theme(
-    text = element_text(size = 16)
-    , axis.line = element_blank()
-    , axis.text = element_blank()
-    , axis.ticks = element_blank()
-    , legend.position = "top"
-  ) +
-  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
-
-cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+# dataL1 = data %>% 
+#   dplyr::filter(name == "행태 하위요소 비율")
+# 
+# mainTitle = sprintf("%s", "전문가에 따른 행태 하위요소 비율")
+# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
+# 
+# ggplot(dataL1, aes(x = '', y = val, fill = key, label = sprintf("%s\n%s %%", key, round(val, 2)) )) +
+#   geom_bar(stat = 'identity', alpha = 0.6) +
+#   coord_polar('y', start = 0) +
+#   ggrepel::geom_label_repel(position = position_stack(vjust = 0.5), color = "white", size = 5, alpha = 0.8, show.legend = FALSE) +
+#   labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = mainTitle) +
+#   theme_minimal() +
+#   theme_classic() +
+#   theme(
+#     text = element_text(size = 16)
+#     , axis.line = element_blank()
+#     , axis.text = element_blank()
+#     , axis.ticks = element_blank()
+#     , legend.position = "top"
+#   ) +
+#   ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+# 
+# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
