1228ceaa8ed45ba2566e968fe50363487ce4843a
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2023-12-14 07:46:11 +0900

    Update TalentPlatform-LSH0529.R

--- src/TalentPlatform-LSH0529.R
+++ src/TalentPlatform-LSH0529.R
@@ -52,24 +52,19 @@ library(formattable)
 library(webshot)
 library(openxlsx)
 library(forcats)
-
+library(extrafont)
+library(magick)
 
 # 파일 읽기
 fileInfo = Sys.glob(file.path(globalVar$inpPath, serviceName, "LSH0529. R을 이용한 학위 논문 표 및 그림 시각화.xlsx"))
 
+# ================================================
 # 그림 생성
+# ================================================
 figData = openxlsx::read.xlsx(fileInfo, sheet = "그림") %>% 
   as.tibble()
 
-
 # figData$name %>% unique()
-# 난방 총 용량의 건물에너지성능 영향정도
-# 냉난방설비 종류의 건물에너지성능 영향정도
-# 신재생에너지 종류의 건물에너지성능 영향정도
-# 태양광 효율의 건물에너지성능 영향정도
-# EHP의 난방 COP값의 건물에너지성능 영향정도
-# 전열교환기 난방효율의 건물에너지성능 영향정도
-
 # 외벽 열관류율의 냉난방에너지 영향정도
 # 지붕 열관류율의 냉난방에너지 영향정도
 # 바닥 열관류율의 냉난방에너지 영향정도
@@ -91,6 +86,8 @@ legList = figDataL1$leg %>% unique() %>% sort(decreasing = TRUE)
 figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling"))
 figDataL1$leg = forcats::fct_relevel(figDataL1$leg, legList)
 
+summary(figDataL1)
+
 mainTitle = sprintf("%s", "열관류율의 냉난방에너지 영향정도")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
 dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
@@ -99,7 +96,7 @@ ggplot(figDataL1, aes(x = leg, y = val, color = key, group = key, label = round(
   geom_line() +
   geom_point() +
   labs(x = NULL, y = "냉난방에너지 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
-  # scale_y_continuous(minor_breaks = seq(0, 50, 5), breaks=seq(0, 40, 5), limits = c(30, 40)) +
+  scale_y_continuous(minor_breaks = seq(0, 50, 5), breaks=seq(0, 50, 5), limits = c(20, 40)) +
   theme(
     text = element_text(size = 16)
     , legend.position = "top"
@@ -108,7 +105,7 @@ ggplot(figDataL1, aes(x = leg, y = val, color = key, group = key, label = round(
   facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
   ggsave(filename = saveImg, width = 8, height = 10, dpi = 600)
 
- cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
 
 # 난방 총 용량의 건물에너지성능 영향정도
@@ -127,6 +124,8 @@ legList = figDataL1$leg %>% unique() %>% sort(decreasing = TRUE)
 # 정렬
 figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling"))
 figDataL1$leg = forcats::fct_relevel(figDataL1$leg, legList)
+
+summary(figDataL1)
 
 mainTitle = sprintf("%s", "난방 총 용량의 건물에너지성능 영향정도")
 saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
@@ -147,36 +146,290 @@ ggplot(figDataL1, aes(x = leg, y = val, color = key, group = key, label = round(
 
 cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
+# 냉난방설비 종류의 건물에너지성능 영향정도
+figDataL1 = figData %>% 
+  dplyr::filter(name == "냉난방설비 종류의 건물에너지성능 영향정도") %>% 
+  dplyr::mutate(
+    key2 = ifelse(is.na(key2), "", key2)
+    # , leg = sprintf("%s\n%s", key2, key)
+    # , leg = sprintf("%s (%s)", key2, key)
+    , leg = sprintf("%s", key)
+  ) %>% 
+  dplyr::select(-c(name, key, key2)) %>%
+  tidyr::pivot_longer(cols = c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"), names_to = "key", values_to = "val")
+
+# 정렬
+figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"))
+figDataL1$leg = forcats::fct_relevel(figDataL1$leg, c("ST.", "EHP", "GHP", "Geo"))
+
+# summary(figDataL1)
+
+mainTitle = sprintf("%s", "냉난방설비 종류의 건물에너지성능 영향정도")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggplot(figDataL1, aes(x = key, y = val, fill = leg, group = leg, label = round(val, 2))) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = NULL, y = "건물에너지성능 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+  scale_y_continuous(minor_breaks = seq(0, 300, 50), breaks=seq(0, 300, 50), limits = c(0, 250)) +
+  theme(
+    text = element_text(size = 16)
+    , legend.position = "top"
+  ) +
+  facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
+  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
 
 
+
+# 신재생에너지 종류의 건물에너지성능 영향정도
+figDataL1 = figData %>% 
+  dplyr::filter(name == "신재생에너지 종류의 건물에너지성능 영향정도") %>% 
+  dplyr::mutate(
+    key2 = ifelse(is.na(key2), "", key2)
+    # , leg = sprintf("%s\n%s", key2, key)
+    # , leg = sprintf("%s (%s)", key2, key)
+    , leg = sprintf("%s", key)
+  ) %>% 
+  dplyr::select(-c(name, key, key2)) %>%
+  tidyr::pivot_longer(cols = c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"), names_to = "key", values_to = "val")
+
+# 정렬
+figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"))
+figDataL1$leg = forcats::fct_relevel(figDataL1$leg, c("ST.", "PV", "Geo"))
+
+# summary(figDataL1)
+
+mainTitle = sprintf("%s", "신재생에너지 종류의 건물에너지성능 영향정도")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggplot(figDataL1, aes(x = key, y = val, fill = leg, group = leg, label = round(val, 2))) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = NULL, y = "건물에너지성능 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+  scale_y_continuous(minor_breaks = seq(0, 300, 50), breaks=seq(0, 300, 50), limits = c(0, 250)) +
+  theme(
+    text = element_text(size = 16)
+    , legend.position = "top"
+  ) +
+  facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
+  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+
+# 태양광 효율의 건물에너지성능 영향정도
+figDataL1 = figData %>% 
+  dplyr::filter(name == "태양광 효율의 건물에너지성능 영향정도") %>% 
+  dplyr::mutate(
+    key2 = ifelse(is.na(key2), "", key2)
+    # , leg = sprintf("%s\n%s", key2, key)
+    , leg = sprintf("%s (%s)", key2, key)
+    # , leg = sprintf("%s", key)
+  ) %>% 
+  dplyr::select(-c(name, key, key2)) %>%
+  tidyr::pivot_longer(cols = c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"), names_to = "key", values_to = "val")
+
+# 정렬
+legList = figDataL1$leg %>% unique() %>% sort(decreasing = FALSE)
+
+figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"))
+figDataL1$leg = forcats::fct_relevel(figDataL1$leg, legList)
+
+# summary(figDataL1)
+
+mainTitle = sprintf("%s", "태양광 효율의 건물에너지성능 영향정도")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggplot(figDataL1, aes(x = key, y = val, fill = leg, group = leg, label = round(val, 2))) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = NULL, y = "건물에너지성능 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+  scale_y_continuous(minor_breaks = seq(0, 300, 50), breaks=seq(0, 300, 50), limits = c(0, 250)) +
+  theme(
+    text = element_text(size = 16)
+    , legend.position = "top"
+    , legend.key.size = unit(0.5, "cm")
+    # , axis.text.x = element_text(angle = 45, hjust = 1, size = 14)
+  ) +
+  guides(fill = guide_legend(nrow = 1)) +
+  facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
+  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+# EHP의 난방 COP값의 건물에너지성능 영향정도
+figDataL1 = figData %>% 
+  dplyr::filter(name == "EHP의 난방 COP값의 건물에너지성능 영향정도") %>% 
+  dplyr::mutate(
+    key2 = ifelse(is.na(key2), "", key2)
+    # , leg = sprintf("%s\n%s", key2, key)
+    , leg = sprintf("%s (%s)", key2, key) %>% stringr::str_trim(side = "both")
+    # , leg = sprintf("%s", key)
+  ) %>% 
+  dplyr::select(-c(name, key, key2)) %>%
+  tidyr::pivot_longer(cols = c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"), names_to = "key", values_to = "val")
+
+# 정렬
+# legList = figDataL1$leg %>% unique() %>% sort(decreasing = TRUE)
+legList = c("5.25 (H-COP 1)", "4.9 (H-COP 2)",   "4.55 (H-COP 3)", "4.2 (H-COP 4)", "3.85 (H-COP 5)", "(ST.)", "3.15 (H-COP 6)", "2.8 (H-COP 7)", "2.45 (H-COP 8)", "2.1 (H-COP 9)", "1.75 (H-COP 10)")
+
+figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"))
+figDataL1$leg = forcats::fct_relevel(figDataL1$leg, legList)
+
+# summary(figDataL1)
+
+mainTitle = sprintf("%s", "EHP의 난방 COP값의 건물에너지성능 영향정도")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggplot(figDataL1, aes(x = key, y = val, fill = leg, group = leg, label = round(val, 2))) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = NULL, y = "건물에너지성능 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+  scale_y_continuous(minor_breaks = seq(0, 300, 50), breaks=seq(0, 300, 50), limits = c(0, 250)) +
+  theme(
+    text = element_text(size = 16)
+    , legend.position = "top"
+    # , legend.key.size = unit(0.4, "cm")
+    # , axis.text.x = element_text(angle = 45, hjust = 1, size = 14)
+  ) +
+  guides(fill = guide_legend(nrow = 3)) +
+  facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
+  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+
+# 전열교환기 난방효율의 건물에너지성능 영향정도
+figDataL1 = figData %>% 
+  dplyr::filter(name == "전열교환기 난방효율의 건물에너지성능 영향정도") %>% 
+  dplyr::mutate(
+    key2 = ifelse(is.na(key2), "", key2)
+    # , leg = sprintf("%s\n%s", key2, key)
+    , leg = sprintf("%s (%s)", key2, key) %>% stringr::str_trim(side = "both")
+    # , leg = sprintf("%s", key)
+  ) %>% 
+  dplyr::select(-c(name, key, key2)) %>%
+  tidyr::pivot_longer(cols = c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"), names_to = "key", values_to = "val")
+
+# 정렬
+# legList = figDataL1$leg %>% unique() %>% sort(decreasing = FALSE)
+legList = c("60 (H-EX-1)", "(ST.)", "65 (H-EX-2)", "70 (H-EX-3)", "75 (H-EX-4)", "80 (H-EX-5)")
+
+figDataL1$key = forcats::fct_relevel(figDataL1$key, c("Heating", "Cooling", "Hot.Water", "Lighting", "Ventilation", "PEP", "PEC", "IR"))
+figDataL1$leg = forcats::fct_relevel(figDataL1$leg, legList)
+
+# summary(figDataL1)
+
+mainTitle = sprintf("%s", "전열교환기 난방효율의 건물에너지성능 영향정도")
+saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+
+ggplot(figDataL1, aes(x = key, y = val, fill = leg, group = leg, label = round(val, 2))) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = NULL, y = "건물에너지성능 영향정도", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+  scale_y_continuous(minor_breaks = seq(0, 300, 50), breaks=seq(0, 300, 50), limits = c(0, 250)) +
+  theme(
+    text = element_text(size = 16)
+    , legend.position = "top"
+    , legend.key.size = unit(0.4, "cm")
+    # , axis.text.x = element_text(angle = 45, hjust = 1, size = 14)
+  ) +
+  guides(fill = guide_legend(nrow = 1)) +
+  facet_wrap(~ name2, scale = "free_x", ncol = 2) + 
+  ggsave(filename = saveImg, width = 10, height = 8, dpi = 600)
+
+cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+
+# ================================================
+# 표 생성
+# ================================================
 tabData = openxlsx::read.xlsx(fileInfo, sheet = "표") %>% 
   as.tibble()
 
-
-
-
-
-
-# 
-# my_table = mtcars[1:8, 1:8] %>%
-#   dplyr::mutate(
-#     hp = color_bar("lightblue")(hp)
-#     , wt = cell_spec(wt, "html", color = ifelse(wt > 2, "red", "blue"))
-#     , vs = cell_spec(vs, "html", color = "white", align = "c", background = factor(vs, c(0, 1), c("#666666", "#BBBBBB")))
-#     # , cyl = color_tile(c(rainbow(3)d))(cyl)
-#     , cyl = color_tile("white", "orange")(cyl)
-#     ) %>% 
-#   kbl(escape = FALSE) %>%
-#   kable_paper(c("striped"), full_width = FALSE) %>%
-#   column_spec(2, color = spec_color(mtcars$mpg[1:8])) %>%
-#   column_spec(6, color = "white",
-#               background = spec_color(mtcars$drat[1:8], end = 0.7),
-#               popover = paste("am:", mtcars$am[1:8]))
-# my_table
-# 
-# 
-# html_table <- "my_table.html"
-# writeLines(as.character(my_table), html_table)
-# webshot(html_table, "my_table.png", delay = 2) 
-
-
+nameList = tabData$name %>% unique() %>% sort(decreasing = TRUE)
+for (nameInfo in nameList) {
+  cat(sprintf("[CHECK] nameInfo : %s", nameInfo), "\n")
+  
+  # 제로에너지건축물 인증 1~5등급을 만족하는 기술요소 조합
+  tabDataL1 = tabData %>% 
+    dplyr::filter(name == nameInfo) %>% 
+    dplyr::select(c(type, id, id2, key2, PV, IR)) %>%
+    tidyr::pivot_longer(cols = c("PV", "IR"), names_to = "key", values_to = "val") %>% 
+    dplyr::mutate(
+      leg = sprintf("(%s) %s", type, key) %>% stringr::str_trim(side = "both")
+    )
+  
+  legList = tabDataL1$leg %>% unique()
+  id2List = tabDataL1$id2 %>% unique()
+  idList = tabDataL1$id %>% unique()
+  
+  # 정렬
+  tabDataL1$leg = forcats::fct_relevel(tabDataL1$leg, legList)
+  tabDataL1$id2 = forcats::fct_relevel(tabDataL1$id2, id2List)
+  tabDataL1$id = forcats::fct_relevel(tabDataL1$id, idList)
+  
+  summary(tabDataL1)
+  
+  mainTitle = sprintf("%s", nameInfo)
+  saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
+  dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+  
+  makePlot = ggplot(tabDataL1, aes(x = id2, y = val, color = id, group = id, label = round(val, 2))) + 
+    geom_line() +
+    geom_point() +
+    labs(x = NULL, y = "기술 요소", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
+    # scale_y_continuous(minor_breaks = seq(0, 50, 5), breaks=seq(0, 50, 5), limits = c(30, 50)) +
+    theme(
+      text = element_text(size = 16)
+      , legend.position = "top"
+      , legend.key.size = unit(0.45, "cm")
+      , axis.text.x = element_text(angle = 45, hjust = 1, size = 12)
+    ) +
+    guides(color = guide_legend(nrow = 2)) +
+    facet_wrap(~ leg, scale = "free_y", ncol = 1)
+  ggsave(makePlot, filename = saveImg, width = 10, height = 8, dpi = 600)
+  
+  cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+  
+  
+  # 제로에너지건축물 인증 1~5등급을 만족하는 기술요소 조합 세부
+  tabDataL2 =  tabData %>% 
+    dplyr::filter(
+      name == nameInfo
+      , ! is.na(key2)
+    ) %>% 
+    dplyr::mutate(
+      leg = sprintf("(%s) %s", type, id) %>% stringr::str_trim(side = "both")
+      , key2Fac = factor(key2, levels = c("패시브, 액티브, 신재생 최소", "패시브 최대, 액티브, 신재생 최소", "패시브, 액티브 최대, 신재생 최소"))
+    ) %>% 
+    dplyr::arrange(key2Fac) %>%
+    tibble::column_to_rownames("leg") %>% 
+    dplyr::select(key2, id2, PV, IR)
+  
+  subTitle = sprintf("%s 세부", nameInfo)
+  saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, subTitle)
+  dir.create(dirname(saveImg), showWarnings = FALSE, recursive = TRUE)
+  tmpImg = tempfile(fileext = ".png")
+  
+  tabDataL3 = tabDataL2 %>% 
+    dplyr::mutate(
+      IR = formattable::color_tile("white", "orange")(IR)
+      , key2 = kableExtra::cell_spec(key2, "html", align = "c", color = factor(key2, c("패시브, 액티브, 신재생 최소", "패시브, 액티브 최대, 신재생 최소", "패시브 최대, 액티브, 신재생 최소"), c("red", "green", "blue")))
+    ) %>% 
+    kableExtra::kbl(escape = FALSE, row.names = TRUE, col.names = c("Desc.", "Comb.", "PV Capa. (kW)", "IR (%)")) %>%
+    kableExtra::kable_paper(c("striped"), full_width = FALSE) %>% 
+    kableExtra::column_spec(4, color = "white", background = spec_color(tabDataL2$PV, end = 0.7)) %>% 
+    kableExtra::save_kable(file = tmpImg, density = 600, zoom = 10)
+  
+  # file_move(tmpImg, saveImg)
+  
+  # 이미지 여백 제거
+  tmpImg %>% 
+    magick::image_read() %>% 
+    magick::image_trim() %>%
+    magick::image_write(path = saveImg)
+  
+  cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
+}
