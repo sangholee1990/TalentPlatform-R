ed16dbafd241ba16699ba199883d739d4cedada2
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2024-03-08 09:44:56 +0900

    commit

--- src/TalentPlatform-LSH0546.R
+++ src/TalentPlatform-LSH0546.R
@@ -18,6 +18,19 @@
 # 다만 scatterplot 및 95% confidence boundary 를 나타낸 그림이 필요한데, 다른 분께서 진행하신 기존 작업물들이 다 R program 으로 그리셨더라구요.
 # 그래서 R program 으로 제작의뢰드려고 하는데 가능한지 궁금합니다. figure 예시 첨부드립니다. 감사합니다.
 
+# 1. Group 명을 아래와 같이 변경해주세요.
+# 
+# 0 -> Maxilla-first
+# 1 -> Mandible-first
+# 2 -> Mandible only
+# 
+# 2. Group 색깔을 현재 보내주신 것에서 1번과 2번을 바꿔주세요.
+# 
+# 3. 95% confidence boundary 는 점선이 되게 해주세요. 그리고 예시처럼 점선이 조금만 더 촘촘하게 해주세요.
+# 
+# 
+# 4. 현재 그래프가 좀 큰 것 같습니다 (제가 보내드린 예시 그래프와 비교해보면 축 눈금에서 4mm보다 큰 부위와 -4mm 보다 작은 부위 여유공간이 적어보입니다.). 예시 보내주신 것과 같이 해주시면 감사하겠습니다.
+
 # ================================================
 # 초기 환경변수 설정
 # ================================================
@@ -55,6 +68,10 @@ library(tidyverse)
 library(openxlsx)
 library(rlang)
 library(magrittr)
+library(scales)
+
+
+ggplotDefaultColor = scales::hue_pal()(3)
 
 # ================================================
 # 파일 읽기
@@ -74,17 +91,22 @@ for (typeInfo in typeList) {
     dataL1 = data %>%
       dplyr::filter(
         type == typeInfo
+      ) %>% 
+      dplyr::rename(
+        X = x
+        , Y = y
+        , Z = z
       )
     
     statData = dataL1 %>% 
       dplyr::group_by(group) %>% 
       dplyr::summarise(
-        meanX = mean(x, na.rm = TRUE)
-        , meanY = mean(y, na.rm = TRUE)
-        , meanZ = mean(z, na.rm = TRUE)
-        , sdX = sd(x, na.rm = TRUE)
-        , sdY = sd(y, na.rm = TRUE)
-        , sdZ = sd(z, na.rm = TRUE)
+        meanX = mean(X, na.rm = TRUE)
+        , meanY = mean(Y, na.rm = TRUE)
+        , meanZ = mean(Z, na.rm = TRUE)
+        , sdX = sd(X, na.rm = TRUE)
+        , sdY = sd(Y, na.rm = TRUE)
+        , sdZ = sd(Z, na.rm = TRUE)
       )
     
     
@@ -105,128 +127,55 @@ for (typeInfo in typeList) {
         dplyr::select(dplyr::ends_with(colInfo[[2]])) %>%
         magrittr::set_colnames(c("mean", "sd"))
       
-      x = cos(seq(0, 5 * pi, length.out = 1000)) * (1.96 * statDataX$sd) + statDataX$mean
-      y = sin(seq(0, 5 * pi, length.out = 1000)) * (1.96 * statDataY$sd) + statDataY$mean
+      x = cos(seq(0, 5 * pi, length.out = 100)) * (1.96 * statDataX$sd) + statDataX$mean
+      y = sin(seq(0, 5 * pi, length.out = 100)) * (1.96 * statDataY$sd) + statDataY$mean
+
+      tmpData = tibble::tibble(group = groupInfo, x = x, y = y) %>% 
+        dplyr::mutate(angle = atan2(y, x)) %>%
+        dplyr::arrange(angle)
       
-      statDataL1 = dplyr::bind_rows(statDataL1, tibble::tibble(group = groupInfo, x = statDataX$mean, y = statDataY$mean))
-      statDataL2 = dplyr::bind_rows(statDataL2, tibble::tibble(group = groupInfo, x = x, y = y))
+      statDataL1 = dplyr::bind_rows(statDataL1, tibble::tibble(group = groupInfo, x = statDataX$mean, y = statDataY$mean) )
+      statDataL2 = dplyr::bind_rows(statDataL2, tmpData)
     }
     
+    plotSubTitle = sprintf("%s_%s%s-axis-error", typeInfo, colInfo[[1]], colInfo[[2]])
+    saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, plotSubTitle)
+    dir.create(path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
     
-    ggplot() +
+    makePlot = ggplot() +
       geom_hline(yintercept = 0, colour = "grey", linetype = "dashed") + 
       geom_vline(xintercept = 0, colour = "grey", linetype = "dashed") +
-      geom_point(data = dataL1, aes(x, y, colour = group)) +
+      
+      # geom_point(data = dataL1, aes(colInfo[[1]], colInfo[[2]], colour = group)) +
+      geom_point(data = dataL1, aes(x = !!sym(colInfo[[1]]), y = !!sym(colInfo[[2]]), colour = group)) +
+      
       # geom_point(data = statDataL1, aes(x, y, color = group), shape=17, size=3, show.legend = FALSE) +
       geom_point(data = statDataL1 %>% dplyr::filter(group == 0), aes(x, y), color = "red", shape=17, size=3, show.legend = FALSE) +
-      geom_point(data = statDataL1 %>% dplyr::filter(group == 1), aes(x, y), color = "green", shape=17, size=3, show.legend = FALSE) +
-      geom_point(data = statDataL1 %>% dplyr::filter(group == 2), aes(x, y), color = "blue", shape=17, size=3, show.legend = FALSE) +
-      geom_path(data = statDataL2, aes(x, y, colour = group), linetype = "dashed", show.legend = FALSE) +
-      
+      geom_point(data = statDataL1 %>% dplyr::filter(group == 1), aes(x, y), color = "blue", shape=17, size=3, show.legend = FALSE) +
+      geom_point(data = statDataL1 %>% dplyr::filter(group == 2), aes(x, y), color = "green", shape=17, size=3, show.legend = FALSE) +
+      geom_path(data = statDataL2, aes(x, y, colour = group), size = 0.5, linetype = 2, show.legend = FALSE) +
       labs(title = NULL, x = sprintf("%s-axis error (mm)", colInfo[[1]]), y =  sprintf("%s-axis error (mm)", colInfo[[2]]), color = "group") +
-      xlim(-4, 4) +
-      ylim(-4, 4) +
+      # xlim(-5, 5) +
+      # ylim(-5, 5) +
+      scale_x_continuous(minor_breaks = seq(-4, 4, 2), breaks=seq(-4, 4, 2), limits=c(-5, 5)) +
+      scale_y_continuous(minor_breaks = seq(-4, 4, 2), breaks=seq(-4, 4, 2), limits=c(-5, 5)) +
       theme_classic() +
       theme(
         panel.border = element_rect(colour = "black", fill = NA, size = 0.5)
         , legend.background = element_rect(colour = "black", fill = NA, size = 0.5)
-        , legend.position = c(0.95, 0.95)
+        , legend.position = c(0.98, 0.98)
         , legend.justification = c("right", "top")
+      ) +
+      scale_color_manual(
+        name = NULL
+        , na.value = "transparent"
+        , values = c("0" = ggplotDefaultColor[1], "1" = ggplotDefaultColor[3], "2" = ggplotDefaultColor[2])
+        , labels = c("Maxilla-first", "Mandible-first", "Mandible only")
       )
+    
+    ggsave(makePlot, filename = saveImg, width = 5, height = 5, dpi = 600)
+    
+    # shell.exec(saveImg)
+    cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
   }
 }
-
-#   
-#     geom_bar(stat = "identity", position = "dodge") +
-#     geom_text(position = position_dodge(width = 0.9), vjust = 1.5, size = 5, color = "white") +
-#     labs(x = "학년", y = "성관계 비율 [%]", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
-#     theme(
-#       text = element_text(size = 16)
-#       , legend.position = "top"
-#     ) # +
-#     # ggsave(filename = saveImg, width = 10, height = 6, dpi = 600)
-#   
-#   # shell.exec(saveImg)
-#   cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#   
-#   
-# }
-# }
-
-#   
-#   dataL1 = data %>%
-#     dplyr::filter(
-#       type == typeInfo
-#     ) 
-# # }
-# 
-# sxlExrtData = readr::read_csv(fileInfo, locale = locale("ko", encoding = "EUC-KR"))
-# 
-# sxlExrtDataL1 = sxlExrtData %>% 
-#   dplyr::filter(응답자특성별 %in% c("학년별")) %>% 
-#   dplyr::select(응답자특성별2, 남학생경험률, 여학생경험률) %>% 
-#   tidyr::gather(-응답자특성별2, key = "key", value = "val") %>% 
-#   dplyr::mutate(
-#     leg = dplyr::case_when(
-#       stringr::str_detect(key, regex("남학생경험률")) ~ "남성"
-#       , stringr::str_detect(key, regex("여학생경험률")) ~ "여성"
-#     )
-#   )
-# 
-# # 정렬
-# sxlExrtDataL1$응답자특성별2= forcats::fct_relevel(sxlExrtDataL1$응답자특성별2, c("중1", "중2", "중3", "고1", "고2", "고3"))
-# 
-# # 학년 및 성별에 따른 성관계 경험률
-# mainTitle = sprintf("%s", "학년 및 성별에 따른 성관계 경험률")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-# 
-# ggplot(sxlExrtDataL1, aes(x = 응답자특성별2, y = val, fill = leg, group = leg, label = round(val, 2))) + 
-#   geom_bar(stat = "identity", position = "dodge") +
-#   geom_text(position = position_dodge(width = 0.9), vjust = 1.5, size = 5, color = "white") +
-#   labs(x = "학년", y = "성관계 비율 [%]", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
-#   theme(
-#     text = element_text(size = 16)
-#     , legend.position = "top"
-#   ) +
-#   ggsave(filename = saveImg, width = 10, height = 6, dpi = 600)
-# 
-# # shell.exec(saveImg)
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
-#   
-# 
-# # ================================================
-# # 성관계 시작 연령 파일 읽기
-# # ================================================
-# fileInfo2 = Sys.glob(file.path(globalVar$inpPath, serviceName, "성관계_시작_연령_2023.csv"))
-# 
-# bgnAgeSxlIntData = readr::read_csv(fileInfo2, locale = locale("ko", encoding = "EUC-KR"))
-# 
-# bgnAgeSxlIntDataL1 = bgnAgeSxlIntData %>% 
-#   dplyr::filter(! 성별 %in% c("전체")) %>% 
-#   dplyr::select(성별, 평균연령) %>% 
-#   tidyr::gather(-성별, key = "key", value = "val") %>% 
-#   dplyr::mutate(
-#     leg = dplyr::case_when(
-#       stringr::str_detect(성별, regex("남학생")) ~ "남성"
-#       , stringr::str_detect(성별, regex("여학생")) ~ "여성"
-#     )
-#   )
-# 
-# # 성별에 따른 성관계 시작 연령
-# mainTitle = sprintf("%s", "성별에 따른 성관계 시작 연령")
-# saveImg = sprintf("%s/%s/%s.png", globalVar$figPath, serviceName, mainTitle)
-# dir.create(fs::path_dir(saveImg), showWarnings = FALSE, recursive = TRUE)
-# 
-# ggplot(bgnAgeSxlIntDataL1, aes(x = 성별, y = val, fill = leg, group = leg, label = round(val, 2))) + 
-#   geom_bar(stat = "identity", position = "dodge") +
-#   geom_text(position = position_dodge(width = 0.9), vjust = 1.5, size = 5, color = "white") +
-#   labs(x = "성별", y = "성관계 시작 연령", fill = NULL, color = NULL, title = NULL, subtitle = mainTitle) +
-#   theme(
-#     text = element_text(size = 16)
-#     , legend.position = "top"
-#   ) +
-#   ggsave(filename = saveImg, width = 10, height = 6, dpi = 600)
-# 
-# # shell.exec(saveImg)
-# cat(sprintf("[CHECK] saveImg : %s", saveImg), "\n")
